30e75542a8b83457452ac2c6897debd24fbea447
fabien jego <fabien.jego.p@gmail.com>
2024-10-16 12:04:55 +0200

    change put

--- server/services/accountService.js
+++ server/services/accountService.js
@@ -2,71 +2,77 @@ const Account = require("../database/models/accountModel");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
-exports.getAccount = async (req) => {
-  const jwtToken = req.headers.authorization.split(" ")[1];
-  const decodedToken = jwt.decode(jwtToken);
-  const userId = decodedToken.id;
-
-  console.log("ID utilisateur décodé :", userId);
-
-  if (!mongoose.Types.ObjectId.isValid(userId)) {
-    throw new Error("Invalid user ID");
-  }
-  const account = await Account.findOne({ accountID: userId });
-  console.log("Envoi des données du compte :", account);
-  if (!account) {
-    throw new Error("Account not found!");
-  }
-  return account;
-};
-exports.getAllAccounts = async () => {
-  try {
-    const accounts = await Account.find({});
-    return accounts;
-  } catch (error) {
-    throw error;
-  }
-};
 module.exports.createAccount = async (accountData) => {
   try {
     const accounts = Array.isArray(accountData) ? accountData : [accountData];
-
-    const results = await Promise.all(
-      accounts.map(async (data) => {
-        const newAccount = new Account(data);
-        return await newAccount.save();
-      })
-    );
-
+    const results = await Account.insertMany(accounts);
     return results;
   } catch (error) {
-    console.error("Error in accountService.js", error);
-    throw new Error(error);
+    console.error("Error creating account(s):", error.message);
+    throw error;
   }
 };
 
-module.exports.updateAccountNote = async (req) => {
+module.exports.getAccount = async (req) => {
   try {
-    console.log("transaction_id:", req.body.transaction_id);
-    console.log("transactionNote:", req.body.transactionNote);
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) throw new Error("Token missing!");
 
-    const updatedAccount = await Account.findOneAndUpdate(
-      {
-        "account1.transactions._id": req.body.transaction_id,
-      },
-      {
-        $set: {
-          "account1.transactions.$.transactionNote": req.body.transactionNote,
-          "account1.transactions.$.transactionCategory":
-            req.body.transactionCategory,
+    const { id: userId } = jwt.verify(token, process.env.JWT_SECRET);
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      throw new Error("Invalid user ID");
+    }
+
+    const account = await Account.findOne({ accountID: userId });
+    if (!account) throw new Error("Account not found!");
+
+    return account;
+  } catch (error) {
+    console.error("Error fetching account:", error.message);
+    throw error;
+  }
+};
+module.exports.getAllAccounts = async () => {
+  try {
+    return await Account.find({});
+  } catch (error) {
+    console.error("Error fetching all accounts:", error.message);
+    throw error;
+  }
+};
+module.exports.updateAccountNote = async (req) => {
+  console.log("=====> id", req.body.transaction_id);
+  console.log("=====> note", req.body.transactionNote);
+  console.log("=====> category", req.body.transactionCategory);
+
+  try {
+    // Essayer de trouver quel compte contient la transaction
+    const accountPaths = ["account1", "account2", "account3"];
+    let updatedAccount = null;
+
+    for (let path of accountPaths) {
+      updatedAccount = await Account.findOneAndUpdate(
+        { [`${path}.transactions._id`]: req.body.transaction_id },
+        {
+          $set: {
+            [`${path}.transactions.$.transactionNote`]:
+              req.body.transactionNote,
+            [`${path}.transactions.$.transactionCategory`]:
+              req.body.transactionCategory,
+          },
         },
-      },
-      { new: true }
-    );
+        { new: true }
+      );
+
+      if (updatedAccount) {
+        break;
+      }
+    }
 
     if (!updatedAccount) {
       throw new Error("Compte ou transaction non trouvé !");
     }
+
     return updatedAccount;
   } catch (error) {
     throw new Error(
